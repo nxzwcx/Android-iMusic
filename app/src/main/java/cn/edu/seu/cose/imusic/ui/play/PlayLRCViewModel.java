d374597116fa75736afa27aae77dad20f9620fc2
nxzwcx <1219887899@qq.com>
2022-09-16 17:25:21 +0800

    v4.0

--- app/src/main/java/cn/edu/seu/cose/imusic/ui/play/PlayLRCViewModel.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.seu.cose.imusic.ui.play;
-
-import androidx.lifecycle.ViewModel;
-
-public class PlayLRCViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/cn/edu/seu/cose/imusic/util/MusicFilter.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.seu.cose.imusic.util;
-
-//过滤文件，判断是否为音乐文件
-
-public class MusicFilter {
-    public static boolean isMusic(String fileName) {
-        return fileName.matches(("^.*?\\.(mp3|wma|flac)$"));  //正则表达式判断MP3文件
-    }
-}
