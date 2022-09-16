784d335c6ccb51711e4e34e0e26d8140e5f1ddf2
nxzwcx <1219887899@qq.com>
2022-09-16 20:37:11 +0800

    commit test

---
+++ iMusic/app/build/generated/source/buildConfig/release/cn/edu/seu/cose/imusic/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cn.edu.seu.cose.imusic;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "cn.edu.seu.cose.imusic";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
